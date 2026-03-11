cf9386ecae181793270d3decd9c94837d579b3bd
mailor <vvorlovv7799@gmail.com>
2026-03-11 17:32:48 +0200

    Добавил докерфайл и докер компос файлы. Только, из минусов, чтобы запустить на локалке поменяйте в .env Хост с db обратно на localhost

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-htpejgnjs9#m)r3%d5ut2s!uqeuyikli0=j0#u2u$%&15806g+'
+# SECURITY WARNING: keep the secret key used in production secret
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 
 load_dotenv()
